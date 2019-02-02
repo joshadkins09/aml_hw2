4a512745a85a994adda3ecff5fdabb38caa391bb
jadkins <joshadkins09@gmail.com>
2019-02-02 13:48:10 -0800

    input processing working better

--- svm.py
+++ svm.py
@@ -2,13 +2,13 @@
 
 import fileinput
 import sys
+from pprint import pprint
 
-from pprint import pprint as print
+import numpy as np
 
 ###############################################################################
 # load data
 ###############################################################################
-
 '''
 0:  age: continuous.
 2:  fnlwgt: continuous.
@@ -20,24 +20,57 @@ from pprint import pprint as print
 CONTINUOUS_COLUMNS = {0, 2, 4, 10, 11, 12}
 
 
-def get_fields_from_line(line):
-    return [float(field) for count, field in enumerate(line.split(', '))
-            if count in CONTINUOUS_COLUMNS]
+def convert_label(value):
+    if value == '<=50K':
+        return -1
+    elif value == '>50K':
+        return 1
+    else:
+        print('bad label "{}"'.format(value))
+        sys.exit(2)
 
 
-def load_data_from_file(filename):
-    return [get_fields_from_line(line) for line in fileinput.input(filename)]
+def get_data_from_line(split_line, include_labels):
+    data = [
+        float(field) for cnt, field in enumerate(split_line)
+        if cnt in CONTINUOUS_COLUMNS
+    ]
+    if include_labels:
+        return convert_label(split_line[-1]), data
+    else:
+        return data
+
+
+def load_data_from_file(filename, include_labels):
+    return [
+        get_data_from_line(line.rstrip('\n').split(', '), include_labels)
+        for line in fileinput.input(filename)
+    ]
 
 
 def load_data():
-    train = load_data_from_file('train.txt')
-    test = load_data_from_file('test.txt')
-    return train, test
+    train_labels, train_data = zip(*load_data_from_file('train.txt', True))
+    test_data = load_data_from_file('test.txt', False)
+    return (np.array(train_labels), np.array(train_data), np.array(test_data))
+
+
+###############################################################################
+
+
+def f(x, a=0, b=0):
+    return np.multiply(a, x) + b
+
+
+def cost_funtion(x, y, a, b):
+    foo = 1 - np.multiply(y, f(x, a, b))
+    # r1 = np.maximum()
+    print(foo)
 
 
 def main():
     print('running')
-    train, test = load_data()
+    train_labels, train_data, test_data = load_data()
+    print(len(train_labels), len(train_data), len(test_data))
 
 
 if __name__ == "__main__":
