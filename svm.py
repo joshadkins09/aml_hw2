d03a16fbab38ba60227262d98ff2ed43110c5a61
jadkins <joshadkins09@gmail.com>
2019-02-03 21:12:42 -0800

    'working' best accuracy on autograder 75.54 w/ 1, 5, 10

--- svm.py
+++ svm.py
@@ -92,8 +92,8 @@ def load_data_from_file(filename, include_labels):
     ]
 
 
-def load_data():
-    labels_and_data = load_data_from_file('train.txt', True)
+def load_train_data(filename, include_labels=True):
+    labels_and_data = load_data_from_file(filename, include_labels)
     pivot = calculate_pivot(len(labels_and_data), 10)
     train, test = split_data(labels_and_data, pivot, shuffle=False)
     train_labels, train_data = zip(*train)
@@ -104,6 +104,13 @@ def load_data():
     test_data = preprocess(test_data)
     return (np.array(train_labels), np.array(train_data),
             np.array(test_labels), np.array(test_data))
+
+
+def load_test_data(filename, include_labels=True):
+    data = load_data_from_file(filename, include_labels)
+    # preprocess
+    data = preprocess(data)
+    return np.array(data)
 
 
 ###############################################################################
@@ -119,14 +126,17 @@ def preprocess(data):
 
 ###############################################################################
 
-# def f(x, a=0, b=0):
-#     return np.multiply(a, x) + b
+
+def f(x, a, b):
+    return np.multiply(a, x) + b
+
 
 # not sure if we ever need the cost function -jadkins
 # def cost_function(x, y, a, b):
 #     foo = 1 - np.dot(y, f(x, a, b))
 #     r1 = np.maximum(0, foo)
 #     return r1
+
 
 def signof(val):
     if val < 0:
@@ -136,22 +146,23 @@ def signof(val):
 
 class SVM:
     num_seasons = 50
-    m = 1
-    n = 2
 
-    def __init__(self, lam):
+    def __init__(self, lam, m=6, n=10):
         self.lam = lam
+        self.m = m
+        self.n = n
+        self.a = 0
+        self.b = 0
 
-    def f(self, x, a, b):
-        return np.dot(a, x) + b
+    def f(self, x):
+        return np.dot(self.a, x) + self.b
 
     def eta(self, current_step):
         return (self.m / (current_step + self.n))
 
     # this may be private
     def gradient_of_cost(self, x, y, a, b):
-        # not sure if this is the correct type of multiplcation -jadkins
-        v = y * self.f(x, a, b)
+        v = y * self.f(x)
         if v >= 1:
             grad_a = self.lam * a
             grad_b = 0
@@ -160,11 +171,11 @@ class SVM:
             grad_b = -y
         return grad_a, grad_b
 
-    def step(self, x_batch, y_batch, a_est, b_est, step_number):
-        # c = cost_function(x_batch, y_batch, a=a_est, b=b_est)
-        grad_a, grad_b = self.gradient_of_cost(x_batch, y_batch, a_est, b_est)
-        a_new = a_est - self.eta(step_number) * grad_a
-        b_new = b_est - self.eta(step_number) * grad_b
+    def step(self, x_batch, y_batch, a, b, step_number):
+        # c = cost_function(x_batch, y_batch, a=a, b=b)
+        grad_a, grad_b = self.gradient_of_cost(x_batch, y_batch, a, b)
+        a_new = a - self.eta(step_number) * grad_a
+        b_new = b - self.eta(step_number) * grad_b
         return (a_new, b_new)
 
     # def at_each_step(self):
@@ -179,24 +190,12 @@ class SVM:
     #     all_costs.append(math.log(cost_function(x, y, a=a_est, b=b_est)))
 
     def measure_accuracy(self, a, b, holdout_data, holdout_labels):
-        # count = 0
-        res = [signof(self.f(d, a, b)) for c, d in enumerate(holdout_data)]
-        # for predicted, actual in zip(res, holdout_labels):
-        #     print(predicted, actual)
-
+        res = [signof(self.f(d)) for c, d in enumerate(holdout_data)]
         j = [(i, j) for i, j in zip(res, holdout_labels) if i == j]
-        print(len(j)/50)
-        # for predicted, actual in zip(res, holdout_labels):
-        #     print(predicted, actual)
-        # for c, d in enumerate(holdout_data):
-        #     res = self.f(d, a, b)
-        #     print(res, holdout_labels[c])
+        return (len(j) / len(holdout_labels))
 
     def fit(self, train_data, train_labels, num_seasons=50, num_steps=300):
-        # tmp
-        all_a_est, all_b_est = list(), list()
-        a_est = np.zeros([len(train_data[0])])
-        b_est = 0
+        self.a = self.a * np.zeros([len(train_data[0])])
 
         zipped = list(zip(train_data, train_labels))
         for season in range(num_seasons):
@@ -207,23 +206,68 @@ class SVM:
             for step in range(num_steps):
                 # could compute len(data) outside -jadkins
                 n = random.randint(0, len(data) - 1)  # Batch size of 1.
-                a_est, b_est = self.step(data[n], labels[n], a_est, b_est,
-                                         step)
-                all_a_est.append(a_est)
-                all_b_est.append(b_est)
+                self.a, self.b = self.step(data[n], labels[n], self.a, self.b,
+                                           step)
 
                 if step % 30 == 0:
-                    self.measure_accuracy(a_est, b_est, holdout_data,
+                    self.measure_accuracy(self.a, self.b, holdout_data,
                                           holdout_labels)
+
+    def fancy_fit(self,
+                  train_data,
+                  train_labels,
+                  num_seasons=50,
+                  num_steps=300):
+        self.a = self.a * np.zeros([len(train_data[0])])
+        for season in range(num_seasons):
+            data, labels = train_data, train_labels
+            for step in range(num_steps):
+                # could compute len(data) outside -jadkins
+                n = random.randint(0, len(data) - 1)  # Batch size of 1.
+                self.a, self.b = self.step(data[n], labels[n], self.a, self.b,
+                                           step)
+
+    def predict(self, test_data):
+        return signof(self.f(test_data))
+
+
+def fmt_for_sub(val):
+    if val == 1:
+        return '>50k'
+    elif val == -1:
+        return '<=50K'
+    else:
+        print('invlaid value')
+        sys.exit(3)
+
+
+def generate_submission(clf, data):
+    res = [fmt_for_sub(clf.predict(x)) for x in data]
+    with open('submission.txt', 'w') as sub:
+        for r in res:
+            sub.write('{}\n'.format(r))
 
 
 def main():
-    print('running')
-    train_labels, train_data, test_labels, test_data = load_data()
-    svm = SVM(1)
-    print('before')
-    svm.fit(train_data, train_labels)
-    print('after')
+    # print('running')
+    train_labels, train_data, test_labels, test_data = load_train_data(
+        'train.txt', True)
+
+    # for lam in [.0001, .001, .01, .1, 1]:
+    #     svm = SVM(lam, 9, 10)
+    #     svm.fit(train_data, train_labels)
+    #     predictions = [svm.predict(x) for x in test_data]
+    #     print(len(predictions), len(test_labels))
+    #     v = [(a, b) for a, b in zip(predictions, test_labels) if a == b]
+    #     print(len(v) / len(test_labels))
+
+    svm = SVM(1, 5, 10)
+    data = np.concatenate((train_data, test_data))
+    labels = np.concatenate((train_labels, test_labels))
+
+    svm.fancy_fit(data, labels, num_seasons=100)
+    real_test_data = load_test_data('test.txt', False)
+    generate_submission(svm, real_test_data)
 
 
 if __name__ == "__main__":
